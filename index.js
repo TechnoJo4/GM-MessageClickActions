12a89d740afafe7c98ae3f96d0b3a994e7820cdf
Cynthia Foxwell <LUModder@gmail.com>
2021-03-02 17:08:29 -0700

    Update to work on latest discord and clean up code (#1)

--- index.js
+++ index.js
@@ -1,118 +1,131 @@
+import * as webpackModules from "@goosemod/webpack";
+import * as patcher from "@goosemod/patcher";
+import * as settings from "@goosemod/settings";
+
 const name = "Message Click Actions";
 
-let settings = {
-	deleteClick: true,
-	doubleClickEdit: true,
-	controlClickReply: true,
-	keepDiscordBehavior: true
+let moduleSettings = {
+    deleteClick: true,
+    doubleClickEdit: true,
+    controlClickReply: true,
+    keepDiscordBehavior: true,
 };
 
-let keysDown = {
-	Delete: false,
-	Control: false
+const keysDown = {
+    Delete: false,
+    Control: false,
 };
 
-let getListener = v => e => {
-	if (e.key in keysDown)
-		keysDown[e.key] = v;
+const getListener = (v) => (e) => {
+    if (e.key in keysDown) keysDown[e.key] = v;
 };
-let keyDownListener = getListener(true);
-let keyUpListener = getListener(false);
-
-
-let modules = {};
-let getMsgObj = message => message.childrenMessageContent.props.message;
-
-let onClick = message => {
-	if (keysDown.Delete && settings.deleteClick) {
-		const msg = getMsgObj(message);
-		modules.edit.deleteMessage(msg.channel_id, msg.id);
-		return;
-	}
-	if (keysDown.Control && settings.controlClickReply) {
-		const msg = getMsgObj(message);
-		modules.replyToMessage(modules.getChannel(msg.channel_id), msg);
-		return;
-	}
-
-	return true;
-};
-
-let onDoubleClick = message => {
-	if (settings.doubleClickEdit) {
-		const msg = getMsgObj(message);
-		modules.edit.startEditMessage(msg.channel_id, msg.id, msg.content || '');
-	}
-};
-
+const keyDownListener = getListener(true);
+const keyUpListener = getListener(false);
 
 let unpatch;
 export default {
-	goosemodHandlers: {
-		onImport: async () => {
-			let mod = goosemodScope.webpackModules;
-			modules.edit = mod.findByProps("startEditMessage");
-			modules.getChannel = mod.findByProps("getChannel").getChannel;
-			modules.replyToMessage = mod.findByProps("replyToMessage").replyToMessage;
+    goosemodHandlers: {
+        onImport: async () => {
+            const { startEditMessage, deleteMessage } = webpackModules.findByProps("startEditMessage");
+            const { getChannel } = webpackModules.findByProps("getChannel");
+            const { replyToMessage } = webpackModules.findByProps("replyToMessage");
 
-			const messageModule = mod.find(m => m.default && m.default.displayName === "Message");
-			unpatch = goosemodScope.patcher.patch(messageModule, "default", (args) => {
-				let orig = args[0].onClick;
-				args[0].onClick = function() {
-					if (onClick(args[0]) && settings.keepDiscordBehavior)
-						orig.apply(this, arguments);
-				};
+            function doubleClickHandler(props) {
+                if (moduleSettings.doubleClickEdit) {
+                    const msg = props.message;
+                    startEditMessage(
+                        msg.channel_id,
+                        msg.id,
+                        msg.content || ""
+                    );
+                }
+            }
 
-				args[0].onDoubleClick = () => onDoubleClick(args[0]);
-			}, true);
+            function onClickHandler(props) {
+                const msg = props.message;
+                if (keysDown.Delete && moduleSettings.deleteClick) {
+                    deleteMessage(msg.channel_id, msg.id);
+                    return;
+                }
+                if (keysDown.Control && moduleSettings.controlClickReply) {
+                    replyToMessage(getChannel(msg.channel_id), msg);
+                    return;
+                }
 
-			document.addEventListener("keydown", keyDownListener);
-			document.addEventListener("keyup", keyUpListener);
-		},
+                return true;
+            }
 
-		onLoadingFinished: async () => {
-			goosemodScope.settings.createItem(name, [
-				{
-					type: "header",
-					text: "Actions",
-				},
-				{
-					type: "toggle",
-					text: "Double-Click to edit",
-					onToggle: (value) => settings.doubleClickEdit = value,
-					isToggled: () => settings.doubleClickEdit,
-				},
-				{
-					type: "toggle",
-					text: "Control + Click to reply",
-					onToggle: (value) => settings.controlClickReply = value,
-					isToggled: () => settings.controlClickReply,
-				},
-				{
-					type: "toggle",
-					text: "Delete + Click to delete",
-					onToggle: (value) => settings.deleteClick = value,
-					isToggled: () => settings.deleteClick,
-				},
-				{
-					type: "toggle",
-					text: "Keep Default Discord Behavior (Alt+Click to mark unread)",
-					onToggle: (value) => settings.keepDiscordBehavior = value,
-					isToggled: () => settings.keepDiscordBehavior,
-				}
-			]);
-		},
+            const Message = webpackModules.find(
+                (m) =>
+                    m.default &&
+                    typeof m.default === "function" && m.default.toString().includes('"className","compact","zalgo",')
+            );
+            unpatch = patcher.patch(
+                Message,
+                "default",
+                (args) => {
+                    const [props] = args;
+                    let orig = props.onClick;
+                    props.onClick = function () {
+                        if (onClickHandler(props.childrenMessageContent.props) && moduleSettings.keepDiscordBehavior)
+                            orig.apply(this, arguments);
+                    };
 
-		onRemove: async () => {
-			goosemodScope.settings.removeItem(name);
+                    props.onDoubleClick = () => doubleClickHandler(props.childrenMessageContent.props);
+                },
+                true
+            );
 
-			unpatch();
+            document.addEventListener("keydown", keyDownListener);
+            document.addEventListener("keyup", keyUpListener);
+        },
 
-			document.removeEventListener("keydown", keyDownListener);
-			document.removeEventListener("keyup", keyUpListener);
-		},
+        onLoadingFinished: async () => {
+            settings.createItem(name, [
+                {
+                    type: "header",
+                    text: "Actions",
+                },
+                {
+                    type: "toggle",
+                    text: "Double-Click to edit",
+                    onToggle: (value) => (moduleSettings.doubleClickEdit = value),
+                    isToggled: () => moduleSettings.doubleClickEdit,
+                },
+                {
+                    type: "toggle",
+                    text: "Control + Click to reply",
+                    onToggle: (value) => (moduleSettings.controlClickReply = value),
+                    isToggled: () => moduleSettings.controlClickReply,
+                },
+                {
+                    type: "toggle",
+                    text: "Delete + Click to delete",
+                    onToggle: (value) => (moduleSettings.deleteClick = value),
+                    isToggled: () => moduleSettings.deleteClick,
+                },
+                {
+                    type: "toggle",
+                    text:
+                        "Keep Default Discord Behavior (Alt+Click to mark unread)",
+                    onToggle: (value) => (moduleSettings.keepDiscordBehavior = value),
+                    isToggled: () => moduleSettings.keepDiscordBehavior,
+                },
+            ]);
+        },
 
-		getSettings: () => [settings],
-		loadSettings: ([_settings]) => { settings = _settings; }
-	}
+        onRemove: async () => {
+            settings.removeItem(name);
+
+            unpatch();
+
+            document.removeEventListener("keydown", keyDownListener);
+            document.removeEventListener("keyup", keyUpListener);
+        },
+
+        getSettings: () => [moduleSettings],
+        loadSettings: ([_settings]) => {
+            moduleSettings = _settings;
+        },
+    },
 };
